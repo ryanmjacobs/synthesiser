d17c4aa8864db49082f6575bf14e0840830f4775
Ryan Jacobs <ryan@rmj.us>
2018-05-17 04:19:00 -0700

    add leds back

--- src/tb/top_tb.v
+++ src/tb/top_tb.v
@@ -4,15 +4,16 @@ module top_tb();
 
 reg rst = 0;
 reg clk = 0;
+wire [7:0] led;
 wire mclk;
 wire lrck;
 wire sck;
-top top(rst, clk, mclk, lrck, sck);
+top top(rst, clk, led, mclk, lrck, sck);
 
 initial begin
     $display("--- top tb ---");
     $dumpfile("top.vcd");
-    $dumpvars(0, mclk, lrck, sck);
+    $dumpvars(0, led, mclk, lrck, sck);
 end
 
 initial #10000 $finish;
--- src/top.v
+++ src/top.v
@@ -1,14 +1,14 @@
 module top(
     input rst,
 	input clk,
-  //output reg [7:0] led,
+    output reg [7:0] led,
 
     output mclk,
     output lrck,
     output reg sck
 );
 
-//initial led <= 8'b10101010;
+initial led <= 8'b10101010;
 
 // produce dummy data
 initial sck <= 1;
