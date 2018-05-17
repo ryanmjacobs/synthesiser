ed1fad257558bcc26ab92687de0cbb7d885cfe93
Ryan Jacobs <ryan@rmj.us>
2018-05-17 04:17:35 -0700

    dummy waveform

--- src/tb/top_tb.v
+++ src/tb/top_tb.v
@@ -1,14 +1,21 @@
 // 100 MHz scale
 `timescale 10ns/10ns
-module top_tb(
-    output reg clk
-);
+module top_tb();
+
+reg rst = 0;
+reg clk = 0;
+wire mclk;
+wire lrck;
+wire sck;
+top top(rst, clk, mclk, lrck, sck);
 
 initial begin
     $display("--- top tb ---");
     $dumpfile("top.vcd");
-    $dumpvars(0, clk);
+    $dumpvars(0, mclk, lrck, sck);
 end
+
+initial #10000 $finish;
 
 // 100 Mhz clock
 always #1 clk <= ~clk;
--- src/top.v
+++ src/top.v
@@ -1,9 +1,17 @@
 module top(
+    input rst,
 	input clk,
-    output reg [7:0] led,
-    output mclk
+  //output reg [7:0] led,
+
+    output mclk,
+    output lrck,
+    output reg sck
 );
 
-initial led <= 8'b10101010;
+//initial led <= 8'b10101010;
+
+// produce dummy data
+initial sck <= 1;
+clkdiv clkdiv(rst, clk, mclk, lrck);
 
 endmodule
