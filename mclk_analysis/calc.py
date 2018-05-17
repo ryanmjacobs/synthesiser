31f13bf92307e124cbcb0aac5bb96f06b9173f5a
Ryan Jacobs <ryan@rmj.us>
2018-05-17 02:37:38 -0700

    found the smallest error

--- mclk_analysis/calc.py
+++ mclk_analysis/calc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import math
 
 f = open("potential_freqs.txt", "r")
 text = f.read()
@@ -7,3 +8,19 @@ freq_str = list(filter(None, text.replace("\n", " ").split(" ")))
 freq_num = list(map(float, freq_str))
 
 print(freq_num)
+
+o = {}
+for freq in iter(freq_num):
+    div = 100/freq
+    rem = div - math.floor(div)
+
+    o[freq] = rem
+
+print("")
+for freq in iter(o):
+    err = o[freq]
+
+    print("%1.3f Mhz => %0.2f error" % (freq, err))
+
+best = min(o, key=o.get)
+print("\nmin error: %0.3f => %0.2f error" %(best, o[best]))
