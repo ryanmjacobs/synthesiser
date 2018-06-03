4bab9162e683f7bb3bbc3447d03c8191907e8015
Ryan Jacobs <ryan@rmj.us>
2018-06-03 16:17:09 -0700

    we

--- xise/synthesizer/src/top.v
+++ xise/synthesizer/src/top.v
@@ -11,11 +11,16 @@ module top(
 
 initial led <= 8'b10101010;
 
-reg write_enable = 0;
+//reg write_enable = 0;
+wire write_enable = 0;
 wire [15:0] data_read;
 inout [15:0] MemDB;
 
-ram ram_(clk, rst, write_enable, sw, MemDB, data_read,
+wire [15:0] write_data;
+assign write_data = sw;
+assign write_enable = sw[0];
+
+ram ram_(clk, rst, write_enable, write_data, MemDB, data_read,
     RamAdv, RamClk, RamCS, MemOE, MemWR, RamLB, RamUB, MemAdr);
 
 always @(*) begin
