9784ea71288a48b08eae76ad0e5849d8bb53b03d
kevinnnyi <36563011+kevinnnyi@users.noreply.github.com>
2018-06-03 16:21:17 -0700

    moved inout declaration, changed write_enable initialization

--- xise/synthesizer/src/top.v
+++ xise/synthesizer/src/top.v
@@ -4,6 +4,7 @@ module top(
   //output reg [7:0] led,
     output reg [7:0] led,
     input [7:0] sw,
+    inout [15:0] MemDB,
 
     output RamAdv, RamClk, RamCS, MemOE, MemWR, RamLB, RamUB,
     output [22:0] MemAdr
@@ -12,9 +13,9 @@ module top(
 initial led <= 8'b10101010;
 
 //reg write_enable = 0;
-wire write_enable = 0;
+wire write_enable;
 wire [15:0] data_read;
-inout [15:0] MemDB;
+
 
 wire [15:0] write_data;
 assign write_data = sw;
