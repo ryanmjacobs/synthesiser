559c18b81f27144cccee406d35d93e330bfc86eb
Ryan Jacobs <ryan@rmj.us>
2018-06-03 03:46:19 -0700

    ensure 256 ticks
    @mickjacobs
    @ryanmjacobs

--- src/tb/clkdiv_tb.v
+++ src/tb/clkdiv_tb.v
@@ -6,10 +6,12 @@ wire mclk;
 wire lrck;
 clkdiv clkdiv(rst, clk, mclk, lrck);
 
+integer mclk_cnt = 0;
+
 initial begin
     $display("--- clk tb ---");
     $dumpfile("clkdiv.vcd");
-    $dumpvars(0, rst, clk, mclk, lrck);
+    $dumpvars(0, rst, clk, mclk, lrck, mclk_cnt);
 end
 
 initial begin
@@ -18,5 +20,10 @@ initial begin
     #5000 $finish;
 end
 always #1 clk <= ~clk;
+
+always @(posedge clk) begin
+    if (mclk)
+        mclk_cnt <= mclk_cnt + 1;
+end
 
 endmodule
