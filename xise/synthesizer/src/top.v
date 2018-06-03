bb34536caaaafb0d0a86e1d4e869fb3b016aa501
Ryan Jacobs <ryan@rmj.us>
2018-06-03 04:46:50 -0700

    use a single always block
    @mickjacobs

--- xise/synthesizer/src/top.v
+++ xise/synthesizer/src/top.v
@@ -3,8 +3,8 @@ module top(
 	input clk,
     output reg [7:0] led,
 
-    output mclk,
-    output lrck,
+    output reg mclk,
+    output reg lrck,
     output reg sck,
     output reg sdout,
 
@@ -18,7 +18,9 @@ reg lrck;
 reg [3:0]  pos = 0;
 reg [15:0] data = 16'b0001_1000;
 
-clkdiv clkdiv(rst, clk, mclk, _);
+wire mclk_pulse;
+wire lrck_pulse;
+clkdiv clkdiv(rst, clk, mclk_pulse, lrck_pulse);
 
 // sine wave table
 reg [7:0] phase = 0;
@@ -26,33 +28,42 @@ reg	[6:0] sine [0:255];
 assign s = sine[phase];
 initial $readmemh("sine.hex", sine);
 
-// initial states
-initial begin
-    lrck  <= 0;
-    sck   <= 1;
-    sdout <= 0;
+always @(posedge clk) begin
+    // initial states
+    if (rst) begin
+        mclk  <= 0;
+        lrck  <= 0;
+        sck   <= 1;
+        sdout <= 0;
+    end else begin
+        if (mclk_pulse)
+            mclk <= ~mclk;
+
+        if (lrck_pulse)
+            lrck <= ~lrck;
+
+        // reset position when we switch from
+        // Left to Right (or vice versa)
+        if (lrck) begin
+            phase <= phase + 1;
+            data <= sine[phase];
+            data <= sine[phase] + sine[phase] << 8;
+            pos <= 0;
+        end
+
+        // shift the data out on each mclk
+        // TODO: this should be sck?
+        if (mclk) begin
+            sck <= 0;
+            pos <= pos + 1;
+            sdout <= (data >> ~pos);
+
+            if (pos == 15)
+                lrck <= ~lrck;
+        end else begin
+            sck <= 1;
+        end
+    end
 end
-
-// reset position when we switch from
-// Left to Right (or vice versa)
-always @(lrck) begin
-    phase <= phase + 1;
-    data <= sine[phase];
-    data <= sine[phase] + sine[phase] << 8;
-  //data <= 16'b1111_0000_0000_1100;
-    pos <= 0;
-end
-
-always @(posedge mclk) begin
-    sck <= 0;
-    pos <= pos + 1;
-    sdout <= (data >> ~pos);
-
-    if (pos == 15)
-        lrck <= ~lrck;
-end
-
-always @(negedge mclk)
-    sck <= 1;
 
 endmodule
